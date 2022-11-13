5faa91ba837db822838f505bf516f6314c4f735a
Cho Cho Htet <michochohtet2017@gmail.com>
2022-11-13 20:29:38 +0700

    Lesson 2 - Layouts

--- app/src/main/java/com/example/jetpackcomposepratice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepratice/MainActivity.kt
@@ -3,33 +3,52 @@ package com.example.jetpackcomposepratice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcomposepratice.ui.theme.JetpackComposePraticeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard()
+            MessageCard(Message("Cho", "Lesson2"))
 
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard() {
-    Text(text = "Hello Compose")
+fun MessageCard(message: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "android icon ",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        Column {
+            Text(text = message.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = message.body)
+        }
+    }
+
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard()
+    MessageCard(Message("Cho", "Lesson 2"))
 }
 
